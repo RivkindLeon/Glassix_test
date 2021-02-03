0f4b62588b08642aa4f4c7ad6d72c21357cc2d51
Rivkind-Leonid <67498860+Rivkind-Leonid@users.noreply.github.com>
2021-02-03 22:09:52 +0200

    Rivkind_Leonid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
+import {MY_GOOGLE_MAP_API_KEY, MY_IPDATA_API_KEY} from "./utils/constants";
 
 function App() {
+
+    const [latitude,setLatitude] = useState(null)
+    const [longitude,setLongitude] = useState(null)
+    const [userCountry,setUserCountry] = useState(null)
+    const [userCity,setUserCity] = useState(null)
+    const [userFlag,setUserFlag] = useState(null)
+
+    const getLocation =()=>{
+        reloadMap()
+        fetch(`https://api.ipdata.co/?api-key=${MY_IPDATA_API_KEY}`)
+            .then(response => response.json())
+            .then(data =>{
+                setLongitude(data.longitude);
+                setLatitude(data.latitude);
+                setUserCountry(data.country_name);
+                setUserCity(data.city);
+                setUserFlag(data.flag)
+            })
+    }
+    const reloadMap = ()=>{
+        setLongitude(null);
+        setLatitude(null);
+        setUserCountry(null);
+        setUserCity(null);
+        setUserFlag(null)
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+        <div className='text-center'>
+            <h2>Glassix Test Project</h2>
+            <h5>Your Address:
+                {userCountry?<span>{userCountry} </span>:null}
+                {userCity?<span>, {userCity}</span>:null}
+                {userFlag? <img src={`${userFlag}`} alt="flag"/>:null}
+            </h5>
+            <button onClick={getLocation} className='btn btn-success '>{!latitude&&!longitude ?"Get My location":"Update my location"}</button>
+            {longitude&&latitude ?
+                <iframe
+                    className='map d-block'
+                    src={`https://www.google.com/maps/embed/v1/view?zoom=10&center=${latitude},${longitude}&key=${MY_GOOGLE_MAP_API_KEY}`}
+                    >
+                </iframe>:null}
+        </div>
+
     </div>
   );
 }
